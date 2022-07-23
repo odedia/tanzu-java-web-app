ad2d76cc3bed5285724111d6faccd66608b651e6
odedia <odedia@gmail.com>
2022-07-23 22:01:45 +0300

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,7 +21,7 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu!", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu!!!", controller.index());
 
         mockMvc
             .perform(get("/"))
