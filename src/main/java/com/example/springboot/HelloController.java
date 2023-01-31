788efb5bd31eea460fba4ee5705db43fcea8130f
Oded Shopen <odedia@gmail.com>
2023-01-31 14:45:37 +0200

    sham

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
 		Long start = System.currentTimeMillis();
 		logger.debug("Entered greeting method");
 		
-		String  greeting = "Greetings from Spring Boot + Tanzu!!!";
+		String  greeting = "Greetings from Spring Boot + Shaam!!!";
 		
 		logger.debug("Exited greeting method. Total time: {} ms", System.currentTimeMillis()-start);
 
